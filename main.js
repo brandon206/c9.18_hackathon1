c9a9c92a4a2b57f6f61e1d686352c75a40dd844a
Erin Tait <erinmtait@gmail.com>
2018-10-10 14:10:39 -0700

    pseudocoded out display current score function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 function displayCurrentScore(){
-
+    //displays current score by adding up all black gamepieces for the black gamepiece player
+    //displays current score by adding up all white gamepieces for the white gamepiece player
+    //puts these together on the screen
  }
