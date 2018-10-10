52c331e3648fac34703c2e6c756dee071a512c49
Erin Tait <erinmtait@gmail.com>
2018-10-10 14:25:44 -0700

    started jQuery DOM Creation

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(initializeApp);
+
+$("<div>").attr
+
+
+
+
+
+
+
+
+
+
+
+// <div id="grid">
+//     <div class="row">
+//     <div class="cell" ><div class="disc" row="1" col="1"></div></div>
+// <div class="cell" ><div class="disc" row="1" col="2"></div></div>
+// <div class="cell" ><div class="disc" row="1" col="3"></div></div>
+// <div class="cell" ><div class="disc" row="1" col="4"></div></div>
+// <div class="cell" ><div class="disc" row="1" col="5"></div></div>
+// <div class="cell" ><div class="disc" row="1" col="6"></div></div>
+// <div class="cell" ><div class="disc" row="1" col="7"></div></div>
+// <div class="cell" ><div class="disc" row="1" col="8"></div></div>
+// </div>
+// </div>
