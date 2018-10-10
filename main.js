fd9bacc48d27dd7796615f81821f00c923019f39
Brandon Park <brandonpark206@gmail.com>
2018-10-10 13:04:17 -0700

    advanced functionality of possible moves w/ errors

--- main.js
+++ main.js
@@ -1,33 +1,65 @@
 $(document).ready(initializeApp);
-var boardCoordinateArray = [
-    [0,0],[]
-]
+
+var boardCoordinateArray = [];
 var directionArray = [
-    [-1,-1],[1,1],
-    [1,0],[-1,0],
-    [0,-1],[0,1],
-    [-1,1],[1,-1]    
+    [-1,-1],
+    [1,1],
+    [1,0],
+    [-1,0],
+    [0,-1],
+    [0,1],
+    [-1,1],
+    [1,-1]    
 ];
 
 function initializeApp () {
-    addclickhandlers();
+    // addclickhandlers();
+    populateGameboard();
+    // console.log(boardCoordinateArray);
+    findPossibleMoves("discBlack");
 }
-function addclickhandlers () {
-    $(".number_button").on("click",findPossibleMoves);
-}
-function findPossibleMoves () {
-    for(var counter = 0; counter < boardCoordinateArray.length; counter++){
-        if(boardCoordinateArray[i].text() === "o" || "O"){
-            var oppositeColor = boardCoordinateArray[i];
-        }
-        else{
-            return;
-        }
+// function addclickhandlers () {
+//     $(".number_button").on("click",findPossibleMoves);
+// }
+function findPossibleMoves ( currentPlayerColor ) {
+    if(currentPlayerColor==='discWhite'){
+        var oppositeColor = 'discBlack';
+    } else {
+        oppositeColor = 'discWhite';
     }
-    var pieceClick = $(event.currentTarget);
-    for(var i = 0; i < directionArray.length; i++){
-        for(var x = 0; x < directionArray[i].length; x++){
-
+    for(var row = 0; row < boardCoordinateArray.length; row++){
+        for(var column = 0; column < boardCoordinateArray[row].length; column++){
+            $(".selected").removeClass('selected');
+            
+            var targetCell = $(boardCoordinateArray[row][column]);
+            targetCell.addClass('selected');
+            var innerDiscElement = targetCell.find('div');
+            if(innerDiscElement.hasClass( oppositeColor )){
+                console.log('found color');
+                var currentPositionRow = innerDiscElement.attr("row");
+                console.log(currentPositionRow);
+                var currentPositionColumn = innerDiscElement.attr("col");
+                console.log(currentPositionColumn);
+                for(var directionFirstArr = 0; directionFirstArr < directionArray.length; directionFirstArr ++){
+                    var checkRow = [parseInt(currentPositionRow) + parseInt(directionArray[directionFirstArr][0])];
+                    var checkColumn = [parseInt(currentPositionColumn) + parseInt(directionArray[directionFirstArr][1])];
+                    var possibleMove = $(`[row = ${(checkRow)}][col = ${(checkColumn)}]`)
+                    if(possibleMove.hasClass("disc")){
+                        possibleMove.addClass("highlight");
+                    }
+                    //row, column, directionArray, directionFirstArr
+                    //current position: [row, column]
+                    //check position: [row + directionArray[directionFirstArr][0], column + directionArray[directionFirstArr][1]]
+                }
+            }
         }
+
+    }
+}
+function populateGameboard () {
+    var rows = $(".row");
+    for(var rowI = 0; rowI < rows.length; rowI++){
+        var cells = $(rows[rowI]).find(".cell");
+        boardCoordinateArray.push(cells);
     }
 }
