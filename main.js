8567a94cc7fe537c12f56cd53d9298c5448f1d8e
Erin Tait <erinmtait@gmail.com>
2018-10-12 14:25:29 -0700

    skip turn functionality added

--- main.js
+++ main.js
@@ -33,12 +33,10 @@ function generateGameBoard(){
     $("[row='5'][col='5']").addClass('discWhite');
 
 
+    var resetButton = $('<input>').attr({id: 'resetButton', type: 'button', value: 'Reset'});
+    var skipTurn = $('<input>').attr({id: 'skipTurn', type: 'button', value: 'Skip Turn (if no valid moves)'});
 
-
-    //eventually function on this button will be refreshGrid()?
-    var resetButton = $('<input>').attr({id: 'resetButton', type: 'button', value: 'Reset'}).click();
-
-    body.append(resetButton);
+    body.append(resetButton, skipTurn);
 
 }
 
@@ -78,6 +76,7 @@ function populateGameboard () {
 function addclickhandlers () {
     $(".disc").on("click",flipGamePieces);
     $("#resetButton").on("click",resetGame);
+    $("#skipTurn").on("click", skipTurn);
 }
 
 function findPossibleMoves ( startingColor ) {
@@ -201,6 +200,16 @@ function togglePlayers(){
         $('body').css('background-color', 'black');
         $('body').css('color', 'white');
         findPossibleMoves("discBlack");
+    }
+}
+
+function skipTurn(){
+    var testArray = $('.validMoveBorder');
+    console.log(testArray.length);
+    if(testArray.length === 0){
+        togglePlayers();
+    } else {
+        return
     }
 }
 
